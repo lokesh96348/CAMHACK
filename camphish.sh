a3bb1957844f7b04458fd47111554fa4824b5e15
lokesh96348 <134123848+lokesh96348@users.noreply.github.com>
2023-09-01 13:42:05 +0530

    Update and rename camHACK.sh to camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CamPhish v1.5
-# Powered by TechChip
+# Powered by lokesh
 # Credits goes to thelinuxchoice [github.com/thelinuxchoice/]
 
 trap 'printf "\n";stop' 2
@@ -16,7 +16,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CamPhish Ver 1.5 \e[0m \n"
-printf " \e[1;77m  CREATED BY LOKESH
+printf " \e[1;77m lokesh |  \e[0m \n"
 
 printf "\n"
 
